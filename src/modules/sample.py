8aa33286b4664b677420775816a919b5a8efd826
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-04-08 05:36:30 +0800

    (feat): Add upsampling and save outputs

--- src/modules/sample.py
+++ src/modules/sample.py
@@ -16,12 +16,15 @@ class BT2100:
     - [Rec. ITU-R BT.2100](https://www.itu.int/rec/R-REC-BT.2100)
     """
 
-    def subsample_444(self, components: NDArray) -> NDArray:
+    def subsample_444(self, luma: NDArray, chroma: NDArray) -> NDArray:
         """
-        Subsample chroma components using the scheme `4:4:4`
+        Sub-sample chroma components using the scheme `4:4:4`
 
         ## Parameters
-        - `components`: Chroma components
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+            - *Unused*
+        - `chroma`: Chroma components
             - An array with the minimum dimension of 2
             - The shape is represented as `(V, H, ...)`
 
@@ -30,8 +33,8 @@ class BT2100:
 
         ## Details
         - The first (top-left) sample is co-sited with the first Y' or I samples.
-        - Each component has the same number of horizontal
-          samples as the Y' or I component.
+        - Each chrome component has the same number
+          of horizontal samples as the Y' or I component.
 
         ## References
         - Table 8 of Rec. ITU-R BT.2100-2
@@ -39,25 +42,28 @@ class BT2100:
 
         from numpy import asarray
 
-        components = asarray(components)
+        chroma = asarray(chroma)
 
-        if components.ndim < 2:
+        if luma.ndim < 2 or chroma.ndim < 2:
             raise ValueError("The minimum dimension of the components is 2")
 
-        subsampled_components = components[::1, ::1]
-        return subsampled_components
+        subsampled_chroma = chroma[::1, ::1]
+        return subsampled_chroma
 
-    def subsample_422(self, components: NDArray) -> NDArray:
+    def subsample_422(self, luma: NDArray, chroma: NDArray) -> NDArray:
         """
-        Subsample chroma components using the scheme `4:2:2`
+        Sub-sample chroma components using the scheme `4:2:2`
 
         ## Parameters
-        - `components`: Chroma components
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+            - *Unused*
+        - `chroma`: Chroma components
             - An array with the minimum dimension of 2
             - The shape is represented as `(V, H, ...)`
 
         ## Returns
-        - A NumPy array with the shape of `(V, H, ...)`
+        - A NumPy array with the shape of `(V, H / 2, ...)`
 
         ## Details
         - The first (top-left) sample is co-sited with the first Y' or I samples.
@@ -70,25 +76,28 @@ class BT2100:
 
         from numpy import asarray
 
-        components = asarray(components)
+        chroma = asarray(chroma)
 
-        if components.ndim < 2:
+        if luma.ndim < 2 or chroma.ndim < 2:
             raise ValueError("The minimum dimension of the components is 2")
 
-        subsampled_components = components[::1, ::2]
-        return subsampled_components
+        subsampled_chroma = chroma[::1, ::2]
+        return subsampled_chroma
 
-    def subsample_420(self, components: NDArray) -> NDArray:
+    def subsample_420(self, luma: NDArray, chroma: NDArray) -> NDArray:
         """
-        Subsample chroma components using the scheme `4:2:0`
+        Sub-sample chroma components using the scheme `4:2:0`
 
         ## Parameters
-        - `components`: Chroma components
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+            - *Unused*
+        - `chroma`: Chroma components
             - An array with the minimum dimension of 2
             - The shape is represented as `(V, H, ...)`
 
         ## Returns
-        - A NumPy array with the shape of `(V, H, ...)`
+        - A NumPy array with the shape of `(V / 2, H / 2, ...)`
 
         ## Details
         - The first (top-left) sample is co-sited with the first Y' or I samples.
@@ -101,10 +110,112 @@ class BT2100:
 
         from numpy import asarray
 
-        components = asarray(components)
+        chroma = asarray(chroma)
 
-        if components.ndim < 2:
+        if luma.ndim < 2 or chroma.ndim < 2:
             raise ValueError("The minimum dimension of the components is 2")
 
-        subsampled_components = components[::2, ::2]
-        return subsampled_components
+        subsampled_chroma = chroma[::2, ::2]
+        return subsampled_chroma
+
+    def upsample_444(self, luma: NDArray, chroma: NDArray) -> NDArray:
+        """
+        Up-sample chroma components which are sub-sampled using the scheme `4:4:4`
+
+        ## Parameters
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+        - `chroma`: Chroma components
+            - An array with the minimum dimension of 2
+            - The shape is represented as `(V, H, ...)`
+
+        ## Returns
+        - A NumPy array with the shape of `(V, H, ...)`
+
+        ## Details
+        - For sub-sampled input data:
+            - The first (top-left) sample is co-sited with the first Y' or I samples.
+            - Each chrome component has the same number
+              of horizontal samples as the Y' or I component.
+        - This method is not officially defined in the standard
+        """
+
+        from numpy import asarray, repeat
+
+        chroma = asarray(chroma)
+
+        if luma.ndim < 2 or chroma.ndim < 2:
+            raise ValueError("The minimum dimension of the components is 2")
+
+        v, h = luma.shape[:2]
+        upscaled_chroma = repeat(repeat(chroma, 1, axis=0), 1, axis=1)
+        cropped_chroma = upscaled_chroma[:v, :h, ...]
+        return cropped_chroma
+    
+    def upsample_422(self, luma: NDArray, chroma: NDArray) -> NDArray:
+        """
+        Up-sample chroma components which are sub-sampled using the scheme `4:2:2`
+
+        ## Parameters
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+        - `chroma`: Chroma components
+            - An array with the minimum dimension of 2
+            - The shape is represented as `(V, H / 2, ...)`
+
+        ## Returns
+        - A NumPy array with the shape of `(V, H, ...)`
+
+        ## Details
+        - For sub-sampled input data:
+            - The first (top-left) sample is co-sited with the first Y' or I samples.
+            - Horizontally subsampled by a factor of two
+              with respect to the Y' or I component.
+        - This method is not officially defined in the standard
+        """
+
+        from numpy import asarray, repeat
+
+        chroma = asarray(chroma)
+
+        if luma.ndim < 2 or chroma.ndim < 2:
+            raise ValueError("The minimum dimension of the components is 2")
+
+        v, h = luma.shape[:2]
+        upscaled_chroma = repeat(repeat(chroma, 1, axis=0), 2, axis=1)
+        cropped_chroma = upscaled_chroma[:v, :h, ...]
+        return cropped_chroma
+    
+    def upsample_420(self, luma: NDArray, chroma: NDArray) -> NDArray:
+        """
+        Up-sample chroma components which are sub-sampled using the scheme `4:2:0`
+
+        ## Parameters
+        - `luma`: Luma components
+            - An array with the minimum dimension of 2
+        - `chroma`: Chroma components
+            - An array with the minimum dimension of 2
+            - The shape is represented as `(V / 2, H / 2, ...)`
+
+        ## Returns
+        - A NumPy array with the shape of `(V, H, ...)`
+
+        ## Details
+        - For sub-sampled input data:
+            - The first (top-left) sample is co-sited with the first Y' or I samples.
+            - Horizontally and vertically subsampled by a factor of two
+              with respect to the Y' or I component.
+        - This method is not officially defined in the standard
+        """
+
+        from numpy import asarray, repeat
+
+        chroma = asarray(chroma)
+
+        if luma.ndim < 2 or chroma.ndim < 2:
+            raise ValueError("The minimum dimension of the components is 2")
+
+        v, h = luma.shape[:2]
+        upscaled_chroma = repeat(repeat(chroma, 2, axis=0), 2, axis=1)
+        cropped_chroma = upscaled_chroma[:v, :h, ...]
+        return cropped_chroma
--- src/tasks/convert_rgb_to_ycbcr420_and_back.py
+++ src/tasks/convert_rgb_to_ycbcr420_and_back.py
@@ -1,8 +1,12 @@
 from PIL import Image
-from numpy import array, uint8, typing
+from numpy import array, uint8, int32
 from .utils.env import ASSETS_DIR_PATH, OUTPUTS_DIR_PATH
 from ..modules.color import H273, KR_KB_BT601
-from ..modules.data import planar_from_packed, save_ycbcr_image
+from ..modules.data import (
+    packed_from_planar,
+    planar_from_packed,
+    save_ycbcr_image,
+)
 from ..modules.sample import BT2100
 
 image = Image.open(ASSETS_DIR_PATH / "foreman_qcif_0_rgb.bmp")
@@ -22,9 +26,10 @@ assert (
 color = H273()
 
 # Uses ITU-R BT.2100 parameter values
+# - The sub-sampling methods are easier to implement
 sample = BT2100()
 
-# De-quanitze the image to analog RGB
+# De-quanitze the image from digital RGB to analog RGB
 image_data_as_argb = color.set_full_range(True).dequantize_rgb(image_data_as_drgb)
 
 # Convert the image from analog RGB to YPbPr
@@ -39,19 +44,53 @@ image_data_as_y, image_data_as_cb, image_data_as_cr = planar_from_packed(
     image_data_as_ycbcr
 )
 image_data_as_y_subsampled = image_data_as_y.copy()
-image_data_as_cb_subsampled = sample.subsample_420(image_data_as_cb)
-image_data_as_cr_subsampled = sample.subsample_420(image_data_as_cr)
+image_data_as_cb_subsampled = sample.subsample_420(
+    image_data_as_y,
+    image_data_as_cb,
+)
+image_data_as_cr_subsampled = sample.subsample_420(
+    image_data_as_y,
+    image_data_as_cr,
+)
 
 # Save the sub-sampled image in the planar YCbCr format a.k.a. YUV420p
 height, width = image_data_as_y_subsampled.shape
-with open(OUTPUTS_DIR_PATH / f"foreman_qcif_0_ycbcr.yuv420p.{width}x{height}.yuv", "wb") as file:
-    save_ycbcr_image(file, (image_data_as_y, image_data_as_cb, image_data_as_cr))
+with open(
+    OUTPUTS_DIR_PATH / f"foreman_qcif_0_ycbcr.yuv420p.{width}x{height}.yuv",
+    mode="wb",
+) as file:
+    save_ycbcr_image(
+        file,
+        (
+            image_data_as_y_subsampled,
+            image_data_as_cb_subsampled,
+            image_data_as_cr_subsampled,
+        ),
+    )
 
-# Up-sample the image in YCbCr color space using 4:2:0 scheme
-pass
+# Up-sample the image in YCbCr color space from 4:2:0 to 4:4:4 scheme
+image_data_as_y_upsampled = image_data_as_y_subsampled.copy()
+image_data_as_cb_upsampled = sample.upsample_420(
+    image_data_as_y_subsampled,
+    image_data_as_cb_subsampled,
+)
+image_data_as_cr_upsampled = sample.upsample_420(
+    image_data_as_y_subsampled,
+    image_data_as_cr_subsampled,
+)
+image_data_as_ycbcr_upsampled = packed_from_planar((
+    image_data_as_y_upsampled,
+    image_data_as_cb_upsampled,
+    image_data_as_cr_upsampled,
+))
+
+# De-quantize the image from YCbCr to YPbPr
+image_data_as_ypbpr_back = color.set_full_range(False).dequantize_ycbcr(
+    image_data_as_ycbcr_upsampled
+)
 
 # Convert the image from YPbPr to analog RGB
-image_data_as_argb_back = color.rgb_from_ypbpr(image_data_as_ypbpr, kr, kb)
+image_data_as_argb_back = color.rgb_from_ypbpr(image_data_as_ypbpr_back, kr, kb)
 
 # Quantize the image from analog RGB to digital RGB
 image_data_as_drgb_back = color.set_full_range(True).quantize_rgb(
@@ -67,3 +106,10 @@ image_back.save(OUTPUTS_DIR_PATH / f"foreman_qcif_0_rgb.{width}x{height}.bmp")
 assert (
     image.size == image_back.size
 ), "The back image should have the same size as the source image"
+
+a = image_data_as_drgb.astype(int32)
+b = image_data_as_drgb_back.astype(int32)
+r = abs(a - b).max()
+me = (a - b).mean()
+mse = ((a - b) ** 2).mean()
+print(f"R: {r}, ME: {me:.3f}, MSE: {mse:.3f}")
